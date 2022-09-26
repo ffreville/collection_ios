fd90d59f775535b51e53ca3d26dfeaec8336a94a
Frédéric Fréville <52908780+ffreville@users.noreply.github.com>
2022-09-26 20:09:08 +0200

    Init project

---
+++ Collection/CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionApp.swift
+//  Collection
+//
+//  Created by Frédéric Fréville on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
